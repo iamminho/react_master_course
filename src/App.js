a0eb19eaaf5333b2ac29feab599a81095a948e8a
iamminho <minho100227@kakao.com>
2023-06-04 19:08:07 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+// Box 상속
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 const Text = styled.span`
@@ -23,10 +22,10 @@ const Text = styled.span`
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor="teal">
         <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      </Box>
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
