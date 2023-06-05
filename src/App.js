f8a80ee144cab9d4d3748d3d3d4e15351f9c9aba
iamminho <minho100227@kakao.com>
2023-06-05 11:48:06 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -2,52 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  background-color: tomato;
-  animation: ${rotation} 3s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😀</Emoji>
-        <Emoji>🤣</Emoji>
-      </Box>
+      <Title>Theme</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const whiteTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
